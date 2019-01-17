9ee80069260a8f37851d9699f219c5c71a45a679
senntyou <jiangjinbelief@163.com>
2019-01-17 21:12:03 +0800

    update

--- src/index.js
+++ src/index.js
@@ -4,36 +4,18 @@ import 'element-ui/lib/theme-chalk/index.css';
 import Vue from 'vue';
 import Element from 'element-ui';
 
-import './styles/index.scss' // global css
+import './styles/index.scss';
 
-import App from './App'
-import router from './router'
-import store from './store'
+import App from './App';
+import router from './router';
+import store from './store';
 
-import i18n from './lang' // Internationalization
-import './icons' // icon
-import './errorLog' // error log
-import './permission' // permission control
-import './mock' // simulation data
+Vue.use(Element, {size: 'medium'});
 
-import * as filters from './filters' // global filters
-
-Vue.use(Element, {
-  size: Cookies.get('size') || 'medium', // set element-ui default size
-  i18n: (key, value) => i18n.t(key, value)
-})
-
-// register global utility filters.
-Object.keys(filters).forEach(key => {
-  Vue.filter(key, filters[key])
-})
-
-Vue.config.productionTip = false
-
+// eslint-disable-next-line no-new
 new Vue({
   el: '#app',
   router,
   store,
-  i18n,
   render: h => h(App)
-})
+});
--- src/store/index.js
+++ src/store/index.js
@@ -1,23 +1,8 @@
-import Vue from 'vue'
-import Vuex from 'vuex'
-import app from './modules/app'
-import errorLog from './modules/errorLog'
-import permission from './modules/permission'
-import tagsView from './modules/tagsView'
-import user from './modules/user'
-import getters from './getters'
+import Vue from 'vue';
+import Vuex from 'vuex';
 
-Vue.use(Vuex)
+Vue.use(Vuex);
 
-const store = new Vuex.Store({
-  modules: {
-    app,
-    errorLog,
-    permission,
-    tagsView,
-    user
-  },
-  getters
-})
+const store = new Vuex.Store({});
 
 export default store
