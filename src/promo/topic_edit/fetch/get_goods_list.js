c565f01a2e56ab931430b97c3fa4e8d3d3b2a88b
kangyong <kangyongxd@163.com>
2019-07-17 15:09:02 +0800

    :bug:fix(promo/topic): 佛事列表接口添加参数

--- src/promo/topic_edit/fetch/get_goods_list.js
+++ src/promo/topic_edit/fetch/get_goods_list.js
@@ -2,6 +2,15 @@ import seeFetch from 'see-fetch';
 
 const req = {};
 
+const pre = params => ({
+  ...params,
+  isFinish: 0,
+  isTest: 1,
+  pageNumber: 0,
+  pageSize: 1000,
+  status: 0,
+});
+
 const refactor = {
   data: 'data.list',
 };
@@ -15,5 +24,6 @@ seeFetch.config('promo/topicEdit/getGoodsList', {
     '/promo/topic_edit/mock/get_goods_list',
   ],
   req: [req, req],
+  pre: [pre, pre],
   refactor: [refactor, refactor],
 });
