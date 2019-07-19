6acc242df9a3eb13e2b2a1b8bce4d3fbbf42ab8d
kangyong <kangyongxd@163.com>
2019-07-19 10:37:13 +0800

    fix(): ...

--- src/router/routes.js
+++ src/router/routes.js
@@ -115,6 +115,7 @@ const routes = [
         component: () => import('../promo/ci/App'),
         name: '慈光计划',
         meta: { title: '慈光计划', noCache: true },
+        hidden: !0,
       },
       {
         path: 'topic',
