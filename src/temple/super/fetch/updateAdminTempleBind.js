c397a35e2ce0d88e91bf328d2dcd5344d2cf22b1
zhaoyuguang <zhaoyuguang@zizaihome.com>
2019-11-13 10:54:51 +0800

    寺院管理-超级访问 改为多用户绑定访问寺院

---
+++ src/temple/super/fetch/updateAdminTempleBind.js
@@ -0,0 +1,11 @@
+import seeFetch from 'see-fetch';
+
+seeFetch.config('temple/super/updateAdminTempleBind', {
+  method: ['post'],
+  stringify: [!0],
+  url: [
+    '/temple/updateAdminTempleBind',
+    '/temple/super/mock/updateAdminTempleBind',
+    '/temple/super/mock/updateAdminTempleBind',
+  ],
+});
