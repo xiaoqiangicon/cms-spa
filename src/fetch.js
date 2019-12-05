1625a9fe4d5cdb1c9e1c62f23dfcf8135daabc30
senntyou <jiangjinbelief@163.com>
2019-12-05 14:18:44 +0800

    update global fetch

--- src/fetch.js
+++ src/fetch.js
@@ -1,3 +1,4 @@
+import { Notification } from 'element-ui';
 import seeFetch from 'see-fetch';
 
 seeFetch.setEnv(__SEE_ENV__); // eslint-disable-line no-undef
@@ -6,6 +7,14 @@ seeFetch.setEnv(__SEE_ENV__); // eslint-disable-line no-undef
 const post = res => {
   res.success = res.result >= 0 || res.errorCode >= 0;
   res.message = res.msg || '未知错误，请稍后重试';
+
+  if (res.error) {
+    Notification({
+      title: '提示',
+      message: `接口错误，请联系自在家开发人员：url[${res.response.url}], status[${res.response.status}], statusText[${res.response.statusText}]`,
+      duration: 0,
+    });
+  }
 };
 
 seeFetch.config('common', {
