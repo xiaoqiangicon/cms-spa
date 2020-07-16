5f40e1f1169a66bf6b111cf5792dbde0b2dff0c3
liqiang <liqiang@zizaihome.com>
2020-07-16 11:28:15 +0800

    finance/receipt:update

--- src/finance/receipt/fetch/cancel_invoice.js
+++ src/finance/receipt/fetch/cancel_invoice.js
@@ -2,6 +2,7 @@ import seeFetch from 'see-fetch';
 
 seeFetch.config('finance/receipt/cancelInvoice', {
   method: ['post'],
+  stringify: [!0],
   url: [
     '/finance/cancelInvoice',
     '/finance/receipt/mock/cancelInvoice',
