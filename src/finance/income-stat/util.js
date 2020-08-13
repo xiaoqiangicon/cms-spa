4169b4c803057a8e8567716c9a26ec95a58e7b7a
senntyou <jiangjinbelief@163.com>
2020-08-13 14:26:39 +0800

    finance/income-stat 添加支付款项目

--- src/finance/income-stat/util.js
+++ src/finance/income-stat/util.js
@@ -75,3 +75,79 @@ export const makeChartConfig = ({ onlineData, offlineData, year }) => ({
     },
   },
 });
+
+export const makeChartTitle2 = ({ year }) => `${year} 执行款概况`;
+
+export const makeChartConfig2 = ({ incomeData, outcomeData, year }) => ({
+  type: 'bar',
+  data: {
+    labels: months,
+    datasets: [
+      {
+        label: '预算',
+        backgroundColor: chartColors.green,
+        borderColor: chartColors.green,
+        data: incomeData,
+        fill: false,
+      },
+      {
+        label: '支出',
+        backgroundColor: chartColors.blue,
+        borderColor: chartColors.blue,
+        data: outcomeData,
+        fill: false,
+      },
+    ],
+  },
+  options: {
+    responsive: true,
+    title: {
+      display: true,
+      text: makeChartTitle2({ year }),
+    },
+    tooltips: {
+      mode: 'index',
+      intersect: false,
+      displayColors: false,
+    },
+    hover: {
+      mode: 'nearest',
+      intersect: true,
+    },
+    scales: {
+      xAxes: [
+        {
+          stacked: true,
+          display: true,
+          gridLines: {
+            display: false,
+          },
+          scaleLabel: {
+            display: true,
+            labelString: '月',
+          },
+        },
+      ],
+      yAxes: [
+        {
+          stacked: true,
+          display: true,
+          gridLines: {
+            display: false,
+          },
+          scaleLabel: {
+            display: true,
+            labelString: '元',
+          },
+          ticks: {
+            callback(value) {
+              return value > 10000
+                ? `${parseFloat((value / 10000).toFixed(2))}w`
+                : value;
+            },
+          },
+        },
+      ],
+    },
+  },
+});
